a46595e2c04e1d71a8d52232c8dcd3fc65adb691
David Todd <concave@acm.org>
2014-06-16 19:23:57 -0600

    Rename the project dir
    New: MongoTweeter, Old: MongoTwitter

---
+++ MongoTweeter/src/com/mongotweeter/DataPublisher.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package com.mongotweeter;
+
+import java.net.UnknownHostException;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.MongoClient;
+
+/**
+ * @author dave
+ * 
+ */
+public class DataPublisher extends Thread {
+
+	private MongoConnection myConnection;
+	private String myDb;
+	private String myCollection;
+	private DBObject myFilter;
+	private TwitterManager myTwitterManager;
+
+	public DataPublisher(MongoConnection connection, String db,
+			String collection, DBObject filter, TwitterManager twitterManager) {
+		this.myConnection = connection;
+		this.myDb = db;
+		this.myCollection = collection;
+		this.myFilter = filter;
+		this.myTwitterManager = twitterManager;
+	}
+
+	@Override
+	public void run() {
+		MongoClient mongoClient = null;
+		try {
+			// TODO - more of the connection details could be applied here
+			// Establish connection
+			mongoClient = new MongoClient(myConnection.getHost());
+
+			// Get the DB data
+			DB db = mongoClient.getDB(myDb);
+			DBCollection collection = db.getCollection(myCollection);
+			DBCursor cursor = collection.find(myFilter);
+
+			// Publish data
+			while ((!isInterrupted()) && (cursor.hasNext())) {
+				String document = cursor.next().toString();
+				myTwitterManager.update(null, document);
+			}
+
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			// close db connection
+			if (mongoClient != null) {
+				mongoClient.close();
+			}
+		}
+	}
+}
---
+++ MongoTweeter/src/com/mongotweeter/TwitterRules.java
@@ -0,0 +1,58 @@
+/**
+ * 
+ */
+package com.mongotweeter;
+
+/**
+ * Container for the Twitter rules this application must follow.
+ * 
+ * @author dave
+ */
+public class TwitterRules {
+
+	/**
+	 * The fast you can get is a half second
+	 */
+	private static final int MIN_TWEET_FREQEUNCY = 500;
+
+	/**
+	 * Default to a frequency of 2 seconds
+	 */
+	private static final int DEFAULT_TWEET_FREQEUNCY = 2000;
+
+	/**
+	 * Frequency in milliseconds that tweets should be published out on
+	 */
+	private int myTweetFrequency = DEFAULT_TWEET_FREQEUNCY;
+
+	/**
+	 * Constructor
+	 */
+	public TwitterRules() {
+		// do nothing
+	}
+
+	/**
+	 * Getter for the frequency
+	 * 
+	 * @return the frequency of the tweets
+	 */
+	public int getTweetFrequency() {
+		return myTweetFrequency;
+	}
+
+	/**
+	 * Getter for the frequency
+	 * 
+	 * @return the frequency of the tweets
+	 */
+	public void setTweetFrequency(int frequency) {
+		// Check that it is not too fast
+		if (frequency >= MIN_TWEET_FREQEUNCY) {
+			this.myTweetFrequency = frequency;
+		} else {
+			// TODO - log message
+		}
+	}
+
+}
